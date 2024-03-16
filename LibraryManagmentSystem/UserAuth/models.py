78ef3ec60e5e87697fa52f87acd0007092f97651
Edenbel27 <151971602+Edenbel27@users.noreply.github.com>
2024-03-16 03:48:19 -0700

    create the book model and register the app.

--- LibraryManagmentSystem/UserAuth/models.py
+++ LibraryManagmentSystem/UserAuth/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Book(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    
+    class Status(models.TextChoices):
+
+    
+        AVAILABLE = 'available', 'Available'
+        BORROWED = 'borrowed', 'Borrowed'
+    
+    
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.AVAILABLE)
+    created_time = models.DateTimeField(default = timezone.now )
+    
+
+    def __str__(self):
+        return self.title
